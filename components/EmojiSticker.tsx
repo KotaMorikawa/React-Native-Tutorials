44a24c2189817c6b81fb091e10c80ce1c5040cc4
KotaMorikawa <koke.kokota0130@gmail.com>
2025-01-06 17:09:04 +0900

    initial commit

---
+++ components/EmojiSticker.tsx
@@ -0,0 +1,20 @@
+import { type ImageSource, Image } from "expo-image";
+import { View } from "react-native";
+
+type Props = {
+  imageSize: number;
+  stickerSource: ImageSource;
+};
+
+const EmojiSticker = ({ imageSize, stickerSource }: Props) => {
+  return (
+    <View style={{ top: -350 }}>
+      <Image
+        source={stickerSource}
+        style={{ width: imageSize, height: imageSize }}
+      />
+    </View>
+  );
+};
+
+export default EmojiSticker;
---
+++ components/ImageViewer.tsx
@@ -0,0 +1,23 @@
+import { type ImageSource, Image } from "expo-image";
+import { StyleSheet, View } from "react-native";
+
+type Props = {
+  imgSource: ImageSource;
+  selectedImage?: string;
+};
+
+const ImageViewer = ({ imgSource, selectedImage }: Props) => {
+  const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+
+  return <Image source={imageSource} style={styles.image} />;
+};
+
+export default ImageViewer;
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
